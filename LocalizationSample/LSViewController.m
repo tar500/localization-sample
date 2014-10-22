71d3555117c08571a1f89ccad0c5f6a165f37d2e
Taras Palienko <palienko.t@dbbest.com>
2014-10-22 10:33:10 +0300

    Initial files commit

--- LocalizationSample/LSViewController.m
+++ LocalizationSample/LSViewController.m
@@ -10,20 +10,52 @@
 
 @interface LSViewController ()
 
+@property (strong, nonatomic) IBOutlet UIButton *actionButton;
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
+
 @end
 
 @implementation LSViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self localizeInterface];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)localizeInterface {
+    
+    // view controller title
+    self.title = NSLocalizedString(@"Push the button", @"Push the button scene title");
+    
+    // outlets
+    [self.actionButton setTitle:NSLocalizedString(@"Push me", @"'Push me' button title") forState:UIControlStateNormal];
+    self.statusLabel.text = NSLocalizedString(@"You have not pushed the button yet", @"Status message that tells user that button have not been pushed yet");
+    
 }
+
+
+#pragma mark - Actions
+
+- (IBAction)pushMeButtonAction:(UIButton *)sender {
+    
+    NSString * popupTitle = NSLocalizedString(@"Congratulations!", @"Successful button pushing pop-up title");
+    NSString * statusMessage = NSLocalizedString(@"You have successfully pushed the button", @"Successful button pushing pop-up message");
+    
+    UIAlertView * alert = [[UIAlertView alloc]
+                           initWithTitle:popupTitle
+                           message:statusMessage
+                           delegate:nil
+                           cancelButtonTitle:BUTTON_CLOSE
+                           otherButtonTitles:nil];
+    
+    [alert show];
+    
+    self.statusLabel.text = statusMessage;
+    self.statusLabel.textColor = [UIColor greenColor];
+    self.actionButton.enabled = NO;
+    
+}
+
 
 @end
---
+++ LocalizationSample/Strings.h
@@ -0,0 +1,15 @@
+//
+//  Strings.h
+//  LocalizationSample
+//
+//  Created by tar on 21.10.14.
+//  Copyright (c) 2014 tar. All rights reserved.
+//
+
+#ifndef LocalizationSample_Strings_h
+#define LocalizationSample_Strings_h
+
+#define BUTTON_CLOSE NSLocalizedString(@"Close", @"Close button title")
+#define BUTTON_OK NSLocalizedString(@"OK", @"OK button title")
+
+#endif
